f8034e69349be4fd7effa59e51f121ce64e58a22
Shivam Pal <shibru127@gmail.com>
2023-07-14 23:49:32 +0530

    added alternative servel url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL ||
-  //   "https://livebot.shibru1275837.workers.dev/" ||
+  "https://livebot.shibru1275837.workers.dev/" ||
   "http://localhost:8787";
