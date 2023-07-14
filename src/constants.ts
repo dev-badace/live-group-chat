223195270587e0111fe2779b2c319487d90def7b
Shivam Pal <shibru127@gmail.com>
2023-07-14 23:50:07 +0530

    fixed url typo

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL ||
-  "https://livebot.shibru1275837.workers.dev/" ||
-  "http://localhost:8787";
+  "https://livebot.shibru1275837.workers.dev";
